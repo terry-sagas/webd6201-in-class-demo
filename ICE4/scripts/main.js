0e4e61e0a0cf0c5eed9ae5d7398ad8fb10962711
terry-sagas <youremail@yourdomain.com>
2023-02-05 18:44:40 -0500

    added ice 4 folder

--- ICE4/scripts/main.js
+++ ICE4/scripts/main.js
@@ -1,5 +1,5 @@
 (function () {
-    function DisplayButton(){
+    function DisplayHome(){
         let randomButton = document.getElementById("randomButton")
         randomButton.addEventListener("click", function(){
             location.href = './projects.html'
@@ -16,8 +16,7 @@
         mainParagraph.setAttribute("id", "MainParagraph")
         mainParagraph.setAttribute("class", "mt-3 container")
 
-        // concatenation - '1' + '2' + '3'
-        // interpolation - `${var_1}`
+
         let firstString = "This is a "
         let secondString = `${ firstString } main paragraph that we added through javascript`
         mainParagraph.textContent = secondString
@@ -29,43 +28,97 @@
 
         // add after(append)
         mainContent.appendChild(mainParagraph) 
-        
-        // add before (before)
-        // mainContent.before(mainParagraph) 
 
-        // delete element
-        // document.getElementById("RandomButton").remove()
+        // let terry = new Contact("Terry Sagas", "9053912867", "terrysagas@dcmail.ca")
+        // console.log(terry.toString())
 
-        // documentBody.innerHTML = `
-        //     <div class="container">
-        //         <h1 class="display-1">Hello WEBD6201</h1>
-        //         <p class="mt-5 lead">and... what do you this of this method?</p>
-        //     </div>
-        // `
-        let terry = new Contact("Terry Sagas", "9053912867", "terrysagas@dcmail.ca")
-        console.log(terry.toString())
+    }
+    function DisplayProjects(){
+        console.log("Projects page")
+    }
+    function DisplayContacts() {
+        console.log("Contact Us Page")
+
+        let submitButton = document.getElementById("submitButton")
+        let subscribeCheckbox = document.getElementById("subscribeCheckbox")
+
+        // localStorage Example
+        // localStorage.setItem("Random Variable", "random variable for testing and demonstration")
+        // console.log(localStorage.getItem("Random Variable"))
+        // localStorage.removeItem("Random Variable")
+
+        submitButton.addEventListener("click", function() {
+            // event.preventDefault()
+            if (subscribeCheckbox.checked) {
+                // If user subscribes, store the contact in localStorage
+                let contact = new Contact(fullName.value, contactNumber.value, emailAddress.value)
+                if (contact.serialize()) {
+                    let key = contact.Name.substring(0, 1) + Date.now()
+                    localStorage.setItem(key, contact.serialize())
+                }
+            }
+        })
     }
 
-    function Start() {
-        console.log("App Started!")
+    function DisplayContactList() {
+        if (localStorage.length > 0) {
+            let contactList = document.getElementById("contactList") // Our contact list in the table of the contact-list page
 
-        if (document.title === ''){
+            let data = "" // Add data to this variable. Append deserialized data from localStorage to data
+            let keys = Object.keys(localStorage) // Return a String Array of keys
 
+            let index = 1 // Count number of keys
+
+            // for every key in the keys collection
+            for (const key of keys) {
+                let contactData = localStorage.getItem(key) // Get localStorage data value related to the key
+                let contact = new Contact()
+                
+                contact.deserialize(contactData)
+
+                // Inject repeatable row into the contactList
+                data += `<tr>
+                    <th scope="row" class="text-center">${ index }</th>
+                    <td class="text-center">${ contact.Name }</td>
+                    <td class="text-center">${ contact.ContactNumber }</td>
+                    <td class="text-center">${ contact.EmailAddress }</td>
+                    <td class="text-center"></td>
+                    <td class="text-center"></td>
+                </tr>
+                `
+
+                index++
+            }
+
+            contactList.innerHTML = data
         }
-        else{
+    }
 
-        }
-
-        switch(document.title){
-            case "Home - WEBD6201 Demo":
-                DisplayButton()
-                break
-            case "Projects - WEBD6201 Demo":
-                DisplayButton()
-                break
-        
-        }
+    function DisplayReferences() {
+        console.log("References Page")
     }
     
+    function Start() {
+        console.log("App Started Successfully!")
+
+        switch (document.title) {
+            case "Home - WEBD6201 Demo":
+                DisplayHome()
+                break
+            case "Projects - WEBD6201 Demo":
+                DisplayProjects()
+                break
+            case "Contact Us - WEBD6201 Demo":
+                DisplayContacts()
+                break
+            case "Contact List - WEBD6201 Demo":
+                DisplayContactList()
+                break
+            case "References - WEBD6201 Demo":
+                DisplayReferences()
+                break
+        }
+    }
+
     window.addEventListener("load", Start)
 })()
